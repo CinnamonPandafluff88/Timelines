22c448ff9818475e922d3fa6d5e88d1b9d3b9a8f
Sihle <79512045+CinnamonPandafluff88@users.noreply.github.com>
2024-09-02 15:18:00 +0200

    Update main.js

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ function fetchProjectTasks(projectId, tenant) {
     });
 }
 
+
 // Function to fetch all data
 function fetchAllProjectData() {
     const projectUrl = document.getElementById('projectIdInput').value;
